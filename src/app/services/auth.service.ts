cf03885166f89db50ed157d0f6854d9c53bbe889
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-12 18:40:27 -0400

    feat: Nuevos servicios

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class AuthService {
+
+  constructor(
+    private http: HttpService
+  ) { }
+
+  /**
+   * @description Login de usuario
+   * @param data Email y Password
+   */
+  login( data: any ): Observable<any> {
+    return this.http.post( 'endpoint', data );
+    // return this.http.post( '/users/login', data ).pipe(
+    //   map( resp => resp.user as IUser )
+    // );
+  }
+
+  /**
+   * @description Registro de dueño de tienda
+   * @param params Datos del registro
+   */
+  register( params: any ): Observable<any> {
+    return this.http.post( '/users', params );
+  }
+
+  resetPassword( params: any ): Observable<any> {
+    return this.http.post( '/users/reset-password', params );
+  }
+}
