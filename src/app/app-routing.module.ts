bcf2f0d1b0fb8f98ebb6cdd9c86f6eb57ac2265b
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-12 19:24:10 -0400

    feat: Aplicacion de guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShopComponent } from './shop/shop.component';
 import { PagesComponent } from './pages/pages.component';
 import { ElementsComponent } from './elements/elements.component';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // return true;
+
+    if ( this.auth.isAuthenticated() ) {
+      return true;
+    } else {
+      // Redirecciona al login
+      // this.router.navigate( [ 'login' ] );
+      return false;
+    }
   }
-  
+
 }
