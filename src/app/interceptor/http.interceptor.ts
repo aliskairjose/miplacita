1e681e1f27632b4b0f9d1bc70e1dae2654c4ad80
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-12 18:58:26 -0400

    feat: Desarrollo de inerceptor

---
+++ src/app/interceptor/http.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse, 
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable()
+export class HttpInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept( request: HttpRequest<unknown>, next: HttpHandler ): Observable<HttpEvent<unknown>> {
+    // return next.handle(request);
+    const token: string = localStorage.getItem( 'token' );
+    if ( token ) {
+      request = request.clone( { headers: request.headers.set( 'Authorization', 'Bearer ' + token ) } );
+    }
+
+    if ( !request.headers.has( 'Content-Type' ) ) {
+      request = request.clone( { headers: request.headers.set( 'Content-Type', 'application/json' ) } );
+    }
+
+    request = request.clone( { headers: request.headers.set( 'Accept', 'application/json' ) } );
+    return next.handle( request ).pipe(
+      map( ( event: HttpEvent<any> ) => {
+        if ( event instanceof HttpResponse ) {
+          console.log( 'event--->>>', event );
+        }
+        return event;
+      } ),
+      catchError( ( error: HttpErrorResponse ) => {
+        console.log( error );
+        return throwError( error );
+      } )
+    );
+  }
+}
