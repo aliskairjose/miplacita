6216a7a52b0b8eff07da67282d5760bc90cf487d
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-13 14:09:44 -0400

    style: Formato segun TSLINT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,28 @@ import { LoadingBarService } from '@ngx-loading-bar/core';
 import { map, delay, withLatestFrom } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls: [ './app.component.scss' ]
+} )
 export class AppComponent {
-  
+
   // For Progressbar
   loaders = this.loader.progress$.pipe(
-    delay(1000),
-    withLatestFrom(this.loader.progress$),
-    map(v => v[1]),
+    delay( 1000 ),
+    withLatestFrom( this.loader.progress$ ),
+    map( v => v[ 1 ] ),
   );
-  
-  constructor(@Inject(PLATFORM_ID) private platformId: Object,
-    private loader: LoadingBarService, translate: TranslateService) {
-    if (isPlatformBrowser(this.platformId)) {
-      translate.setDefaultLang('en');
-      translate.addLangs(['en', 'fr']);
+
+  constructor(
+    @Inject( PLATFORM_ID ) private platformId: Object,
+    private loader: LoadingBarService,
+    translate: TranslateService
+  ) {
+    if ( isPlatformBrowser( this.platformId ) ) {
+      translate.setDefaultLang( 'en' );
+      translate.addLangs( [ 'en', 'fr' ] );
     }
   }
 
