60cd53b3afde3adb4e8b24a1e96617d687a4ffeb
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-12 23:56:53 -0400

    feat: Metodo lista de producto y detalle de producto

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,18 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Product } from '../interfaces/product';
+import { Observable, Subject } from 'rxjs';
 import { HttpService } from './http.service';
+import { Product } from '../classes/product';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class ProductService {
+
+  product$: Subject<Product> = new Subject<Product>();
 
   constructor(
     private http: HttpService
   ) { }
 
+  /**
+   * @description Lista de productos
+   */
   productList(): Observable<Product[]> {
-    return this.http.get('');
+    return this.http.get( '' );
   }
+
+  /**
+   * @description Detalle de un producto
+   * @param Id Id del producto a consultar
+   */
+  getProduct( id: number ): Observable<Product> {
+    return this.http.get( `/${id}` );
+  }
+
+
+  productSubject( product: Product ): void {
+    this.product$.next( product );
+  }
+
+  productObjerver(): Observable<Product> {
+    return this.product$;
+  }
+
 }
