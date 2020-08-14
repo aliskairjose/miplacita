88a49777e43bc0abe0b4cac39556beeecaa16f1d
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-14 12:09:57 -0400

    style: Se documenta las funciones

--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
+import { Order } from '../classes/order';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class OrderService {
 
-  constructor() { }
+  constructor(
+    private http: HttpService
+  ) { }
+
+  /**
+   * @description Crea una nueva Orden
+   * @param data Data de tipo Order
+   */
+  addOrder( data: Order ): Observable<any> {
+    return this.http.post( 'order', data );
+  }
+
+  /**
+   * @description Retorna el detalle de la orden
+   * @param id Id de la orden
+   */
+  getOrder( id ): Observable<Order> {
+    return this.http.get( `order/${id}` );
+  }
+
+  /**
+   * @description Retorna todas las ordenes de la tienda
+   * @param id Id de la tienda
+   */
+  getAll( id ): Observable<Order[]> {
+    return this.http.get( `order?store=${id}` );
+  }
+
+
+  /**
+   * @description Retorna el listado de clientes del usuario logueado
+   */
+  clientList(): Observable<any> {
+    return this.http.get( 'order/list/clients' );
+  }
 }
