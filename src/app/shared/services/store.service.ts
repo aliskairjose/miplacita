a759312f9b7f35da8d85b745f3427fc39ae8bb55
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-14 12:08:05 -0400

    feat: Desarrollo de endpoints

--- src/app/shared/services/store.service.ts
+++ src/app/shared/services/store.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { Observable } from 'rxjs';
+import { Store } from '../classes/store';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class StoreService {
 
   constructor(
     private http: HttpService
   ) { }
 
-  addStore(data: any): Observable<any> {
-    return this.http.post('stores', data);
+
+  /**
+   * @description Crea una nueva tienda
+   * @param data Data de tipo Store
+   */
+  addStore( data: Store ): Observable<any> {
+    return this.http.post( 'stores', data );
+  }
+
+  /**
+   * @description Retorna el detalle de la tienda
+   * @param id Id de la tienda
+   */
+  getStore( id ): Observable<Store> {
+    return this.http.get( `stores/${id}` );
+  }
+
+  /**
+   * @description Actualiza la data de la tienda
+   * @param id Id de la tienda
+   * @param data Data de tipo Tienda
+   */
+  updateStore( id, data: Store ): Observable<any> {
+    return this.http.put( `storea/${id}`, data );
   }
 }
