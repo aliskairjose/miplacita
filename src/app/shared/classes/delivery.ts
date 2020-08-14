fd27c5ddd26db676ce733a48ef6f09af412a97ac
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-14 13:17:10 -0400

    feat: Desarrollo de metodos

--- src/app/shared/classes/delivery.ts
+++ src/app/shared/classes/delivery.ts
@@ -1,4 +1,4 @@
-export interface DeliveryOptions {
+export interface Delivery{
   id: string;
   name: string;
   phone: string;
---
+++ src/app/shared/services/delivery.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
+import { Delivery } from '../classes/delivery';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class DeliveryService {
+
+  constructor(
+    private http: HttpService
+  ) { }
+
+  /**
+   * @description Agrega Delivery al pedido
+   * @param data Data de tipo Delivery
+   */
+  addDelivery( data: Delivery ): Observable<any> {
+    return this.http.post( 'delivery', data );
+  }
+
+  /**
+   * @description Actualización de la data delivery
+   * @param id Id del delivery a actualizar
+   * @param data Nueva data
+   */
+  updateDelivery( id: string, data: Delivery ): Observable<any> {
+    return this.http.put( `delivery/${id}`, data );
+  }
+
+  /**
+   * @description Elimina el dellivery existente
+   * @param id Id del delivery a eliminar
+   */
+  deleteDelivery(id: string): Observable<any> {
+    return this.http.delete(`delivery/${id}`);
+  }
+
+
+
+}
