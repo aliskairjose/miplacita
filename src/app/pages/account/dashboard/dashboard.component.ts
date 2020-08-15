fb01fb918596e6cf08d6b42f59fcfdfeb4515268
nairelyhz <njhs694@gmail.com>
2020-08-15 18:44:27 -0400

    elementos de dashboard y google charts package

--- src/app/pages/account/dashboard/dashboard.component.ts
+++ src/app/pages/account/dashboard/dashboard.component.ts
@@ -6,12 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  public typeUser = 1;
   public openDashboard: boolean = false;
-
-  constructor() { }
+  public orders = [
+    {
+      name:"pedido 1",
+      status: "pendiente",
+    description: "un pedido x con muchos productos",
+    price: "88.$"
+    }
+  ];
+  public fields = [];
+  public storeFields = [{name: "Pedidos"},
+  {name: "Status"},
+  {name: "Descripción"},
+  {name: "Precio"}]
+  public adminFields = [{name: "Tienda"},
+  {name: "Monto"},
+  {name: "Fecha"},]
+  public stores = [
+    {name: "tienda 1",
+    amount: '45$',
+    date: "12-10-2020"}
+  ]
+  constructor() {
+    if(this.typeUser == 1){
+      this.fields = this.storeFields;
+    }else if(this.typeUser == 2){
+      this.fields = this.adminFields;
+    }  
+  }
 
   ngOnInit(): void {
+
   }
 
   ToggleDashboard() {
