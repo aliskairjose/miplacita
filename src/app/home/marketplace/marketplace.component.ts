dcae764a87721701e8f4f9bd82a8abf5ffd15588
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-17 14:09:08 -0400

    feat: eliminando servicios no usables

--- src/app/home/marketplace/marketplace.component.ts
+++ src/app/home/marketplace/marketplace.component.ts
@@ -118,8 +118,6 @@ export class MarketplaceComponent implements OnInit, OnDestroy {
 
   constructor(
     private _sanitizer: DomSanitizer,
-    private alertService: AlertService,
-    private spinner: NgxSpinnerService,
     public productService: ProductService,
   ) {
     this.productService.getProducts.subscribe( response => {
@@ -137,16 +135,6 @@ export class MarketplaceComponent implements OnInit, OnDestroy {
 
 
   ngOnInit(): void {
-    /*
-      this.alertService.info( 'this is an info alert' );
-      this.alertService.danger( 'this is a danger alert' );
-      this.alertService.success( 'this is a success alert' );
-      this.alertService.warning( 'this is a warning alert' );
-      this.spinner.show();
-      setTimeout(() => {
-        this.spinner.hide();
-      }, 3000);
-    */
     // Change color for this layout
     document.documentElement.style.setProperty( '--theme-deafult', '#e4604a' );
   }
