173a6b3e4fab4cda20955c2f3f4089a1d67b0959
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-17 11:42:14 -0400

    cambio de import

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -40,10 +40,6 @@ import { MasonryGridThreeComponent } from './portfolio/masonry-grid-three/masonr
 import { MasonryGridFourComponent } from './portfolio/masonry-grid-four/masonry-grid-four.component';
 import { MasonryFullWidthComponent } from './portfolio/masonry-full-width/masonry-full-width.component';
 import { AlertModule } from 'ngx-alerts';
-<<<<<<< HEAD
-import { NgxSpinnerModule } from 'ngx-spinner';
-=======
->>>>>>> 09d8598328e5a94a091eb52cb97630593d0a9daf
 
 @NgModule({
   declarations: [
@@ -83,7 +79,6 @@ import { NgxSpinnerModule } from 'ngx-spinner';
   imports: [
     SharedModule,
     CommonModule,
-    NgxSpinnerModule,
     PagesRoutingModule,
     GalleryModule.forRoot(),
     AlertModule.forRoot({maxMessages: 5, timeout: 3000, position: 'left'})
