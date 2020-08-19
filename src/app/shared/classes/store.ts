1804a2cc891de9f395447a40bf07341587fde84d
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-19 15:28:34 -0400

    style: Ajuste de data

--- src/app/shared/classes/store.ts
+++ src/app/shared/classes/store.ts
@@ -5,6 +5,7 @@ export interface Store {
   description: string;
   url_store: string;
   phone: string;
+  currency: string;
   email: string;
   logo: string;
   owner_id: string;
--- src/app/shared/classes/user.ts
+++ src/app/shared/classes/user.ts
@@ -1,6 +1,8 @@
+import { Store } from './store';
 export class User {
   id?: string;
   fullname: string;
   email: string;
   role: string;
+  stores?: Store[];
 }
