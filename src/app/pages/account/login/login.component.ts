5c6dca4de42aaade60a95b960f1d4ab6a3dbb673
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-18 22:19:38 -0400

    feat: observer para el manejo de menu

--- src/app/pages/account/login/login.component.ts
+++ src/app/pages/account/login/login.component.ts
@@ -49,14 +49,11 @@ export class LoginComponent implements OnInit {
           this.storage.setItem( 'token', data.token );
           this.storage.setItem( 'role', data.role );
           this.storage.setItem( 'user', data.user );
-          
+
           this.auth.authSubject( data.success );
 
-          this.alert.info( `Bienvenido ${data.user.fullname}` );
-          setTimeout( () => {
-            // Redireccionamiento al dashboard
-            this.router.navigate( [ 'pages/dashboard' ] );
-          }, 3200 );
+          // Redireccionamiento al dashboard
+          this.router.navigate( [ 'pages/dashboard' ] );
         }
 
       }, ( response: HttpErrorResponse ) => {
