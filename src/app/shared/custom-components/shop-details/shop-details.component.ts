6b569c7e4a9bf10b0a619eb782e95e27e5e902a9
nairelyhz <njhs694@gmail.com>
2020-08-20 11:53:37 -0400

    feat modal con detalles de la suscripcion de la tienda

---
+++ src/app/shared/custom-components/shop-details/shop-details.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-shop-details',
+  templateUrl: './shop-details.component.html',
+  styleUrls: ['./shop-details.component.scss']
+})
+export class ShopDetailsComponent implements OnInit {
+  @ViewChild('shopDetails', { static: false }) ShopDetails: TemplateRef<any>;
+  public benefits = ['10 productos máximos', 'Tienda propia', 'Catálogo virtual'];
+  public modalOpen = false;
+  public modal: any;
+  public shop: any;
+  public plan: any;
+
+  constructor(private modalService: NgbModal) { 
+    console.log("construtor");
+  }
+
+  ngOnInit(): void {
+  }
+
+  openModal(shop) {
+    this.shop = shop;
+    this.getPlanInformation();
+    this.modalOpen = true;
+    this.modal = this.modalService.open(this.ShopDetails);
+  }
+
+  getPlanInformation(){
+    // informacion de plan de la tienda
+    this.plan = { name: 'plan #1'};
+  }
+
+  close(reason){
+    this.modal.close();
+  }
+
+  cancelPlan(){
+    // api para cancelar plan
+  }
+
+  ngOnDestroy() {
+    if(this.modalOpen){
+      this.modalService.dismissAll();
+    }
+  }
+
+}
