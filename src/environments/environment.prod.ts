3ce796e698fbf8675068fbd8414a28c57816f2f8
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-27 19:59:04 -0400

    dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,12 @@ export const environment = {
   stripe_token: 'STRIPE_PUBLISHABLE_KEY',
   paypal_token: 'PAYPAL_CLIENT_ID',
   apiUrl: 'http://marketplace.dev.cronapis.com:3010/api/',
+  orderStatus: [ 'Por aprobar', 'aprobado', 'en proceso', 'cancelado', 'pagado', 'entregado', 'devuelto' ],
   errorForm: {
     invalidEmail: 'Email inválido.',
     required: 'Campo obligatorio.',
     invalidUrl: 'Ingere una url válida.',
+    onlyLetter: 'Solo se permiten caracteres',
+    matchError: 'Los campos deben coincidir'
   }
 };
