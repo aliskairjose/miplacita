1187b8afca67f07d4d6899a5fdda131fbc69ea87
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-27 21:30:12 -0400

    fix

--- src/app/pages/shop-withdrawal/shop-withdrawal.component.ts
+++ src/app/pages/shop-withdrawal/shop-withdrawal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { environment } from '../../../environments/environment.prod';
 
 @Component( {
   selector: 'app-shop-withdrawal',
