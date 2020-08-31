14a9c03a32cb53368d2bdbac16d542033ceea387
nairelyhz <njhs694@gmail.com>
2020-08-31 11:51:26 -0400

    fix dashboard -> totales de ventas,clientes y productos

---
+++ src/app/shared/services/dashboard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { HttpService } from './http.service';
+import { Dashboard } from '../classes/dashboard';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class DashboardService {
+  constructor(
+    private http: HttpService
+  ) { }
+
+  /**
+   * @description Registro de dueño de tienda
+   * @param params Datos del registro
+   */
+  dashboard( ): Observable<any> {
+    return this.http.get( 'dashboard' );
+  }
+
+
+}
