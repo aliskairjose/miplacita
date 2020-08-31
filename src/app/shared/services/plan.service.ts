5ee609dee1647f68a01feff5fbd90826464bbdb8
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-08-31 15:42:56 -0400

    fix: documentacion

--- src/app/shared/services/plan.service.ts
+++ src/app/shared/services/plan.service.ts
@@ -13,18 +13,34 @@ export class PlanService {
     private http: HttpService
   ) { }
 
+  /**
+   * @description Crea un nuevo plan
+   * @param data Data de tipo plan a crear
+   */
   addPlan( data: Plan ): Observable<any> {
     return this.http.post( `plan`, data );
   }
 
+  /**
+   * @description Actualiza los datos del plan
+   * @param id Id del plan a actualizar
+   * @param data Data a actualizar del plan
+   */
   updatePlan( id: string, data: Plan ): Observable<any> {
     return this.http.put( `plan/${id}`, data );
   }
 
+  /**
+   * @description Elimina un plan existente
+   * @param id Id del plan a eliminar
+   */
   deletePlan( id: string ): Observable<any> {
     return this.http.delete( `plan/${id}` );
   }
 
+  /**
+   * @description Obtiene una lista de planes existentes
+   */
   getPlans(): Observable<Plan[]> {
     return this.http.get( 'plan' ).pipe(
       map( response => {
