cf59e9dbf619463c2921590472c15532b3a8a3f5
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-07 15:47:31 -0400

    feat: ajuste de filtro de categorias

--- src/app/shared/components/categories/categories.component.ts
+++ src/app/shared/components/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../../classes/product';
 import { ProductService } from '../../services/product.service';
 import { Category } from '../../classes/category';
@@ -11,20 +11,25 @@ import { Category } from '../../classes/category';
 export class CategoriesComponent implements OnInit {
 
   public products: Product[] = [];
-  public categories: Category[] = [];
   public collapse = true;
 
+  @Input() categories: Category[] = [];
+
   constructor( public productService: ProductService ) {
-    this.getCategories();
   }
 
   ngOnInit(): void {
   }
 
-  getCategories() {
-    this.productService.categoryList().subscribe( ( categories: Category[] ) => {
-      this.categories = [ ...categories ];
-    } );
+  appliedFilter( event ): void {
+
+  }
+
+  // check if the item are selected
+  checked( item ) {
+    // if ( this.categories.indexOf( item ) !== -1 ) {
+    //   return true;
+    // }
   }
 
   // get filterbyCategory() {
--- src/app/shop/collection/widgets/brands/brands.component.ts
+++ src/app/shop/collection/widgets/brands/brands.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../../../shared/classes/tm.product';
 
-@Component({
+@Component( {
   selector: 'app-brands',
   templateUrl: './brands.component.html',
-  styleUrls: ['./brands.component.scss']
-})
+  styleUrls: [ './brands.component.scss' ]
+} )
 export class BrandsComponent implements OnInit {
 
   @Input() products: Product[] = [];
   @Input() brands: any[] = [];
 
   @Output() brandsFilter: EventEmitter<any> = new EventEmitter<any>();
-  
-  public collapse: boolean = true;
 
-  constructor() { 
+  public collapse = true;
+
+  constructor() {
   }
 
   ngOnInit(): void {
@@ -23,29 +23,32 @@ export class BrandsComponent implements OnInit {
 
   get filterbyBrand() {
     const uniqueBrands = [];
-    this.products.filter((product) => {
-      if (product.brand) {
-        const index = uniqueBrands.indexOf(product.brand)
-        if (index === -1) uniqueBrands.push(product.brand)
+    this.products.filter( ( product ) => {
+      if ( product.brand ) {
+        const index = uniqueBrands.indexOf( product.brand );
+        // tslint:disable-next-line: curly
+        if ( index === -1 ) uniqueBrands.push( product.brand );
       }
-    })
-    return uniqueBrands
+    } );
+    return uniqueBrands;
   }
 
-  appliedFilter(event) {
-    let index = this.brands.indexOf(event.target.value);  // checked and unchecked value
-    if (event.target.checked)   
-      this.brands.push(event.target.value); // push in array cheked value
-    else 
-      this.brands.splice(index,1);  // removed in array unchecked value  
-    
-    let brands = this.brands.length ? { brand: this.brands.join(",") } : { brand: null };
-    this.brandsFilter.emit(brands);
+  appliedFilter( event ) {
+    const index = this.brands.indexOf( event.target.value );  // checked and unchecked value
+    if ( event.target.checked ) {
+      this.brands.push( event.target.value );
+    } // push in array cheked value
+    else {
+      this.brands.splice( index, 1 );  // removed in array unchecked value
+    }
+
+    const brands = this.brands.length ? { brand: this.brands.join( "," ) } : { brand: null };
+    this.brandsFilter.emit( brands );
   }
 
   // check if the item are selected
-  checked(item){
-    if(this.brands.indexOf(item) != -1){
+  checked( item ) {
+    if ( this.brands.indexOf( item ) !== -1 ) {
       return true;
     }
   }
