d2a64fe782168183e39af155eb57ef8748fd3756
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-07 09:25:58 -0400

    dropdown menu button search bar

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../classes/category';
 
 @Component( {
   selector: 'app-search',
@@ -9,18 +11,24 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class SearchComponent implements OnInit {
 
   searchForm: FormGroup;
+  categories: Category[];
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService
   ) {
     this.createForm();
   }
 
   ngOnInit(): void {
+    this.categoryService.categoryList().subscribe( ( response: Category[] ) => {
+      this.categories = [ ...response ];
+    } );
   }
 
   onSubmit(): void {
-    
+    console.log( this.searchForm.value );
+    // Conecta con el api de busqueda
   }
 
   // convenience getter for easy access to form fields
