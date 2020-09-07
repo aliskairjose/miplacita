21ccf6756c010fee8158c417f5da0251bc949841
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-07 17:21:09 -0400

    style: comentando codigo no usable

--- src/app/shop/collection/widgets/colors/colors.component.ts
+++ src/app/shop/collection/widgets/colors/colors.component.ts
@@ -1,53 +1,55 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../../../shared/classes/tm.product';
 
-@Component({
+@Component( {
   selector: 'app-colors',
   templateUrl: './colors.component.html',
-  styleUrls: ['./colors.component.scss']
-})
+  styleUrls: [ './colors.component.scss' ]
+} )
 export class ColorsComponent implements OnInit {
 
   @Input() products: Product[] = [];
   @Input() colors: any[] = [];
 
-  @Output() colorsFilter  : EventEmitter<any> = new EventEmitter<any>();
-  
-  public collapse: boolean = true;
+  @Output() colorsFilter: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { 
+  public collapse = true;
+
+  constructor() {
   }
 
   ngOnInit(): void {
   }
 
   get filterbycolor() {
-    const uniqueColors = []
-    this.products.filter((product) => {
-      product.variants.filter((variant) => {
-        if (variant.color) {
-          const index = uniqueColors.indexOf(variant.color)
-          if (index === -1) uniqueColors.push(variant.color)
+    const uniqueColors = [];
+    this.products.filter( ( product ) => {
+      product.variants.filter( ( variant ) => {
+        if ( variant.color ) {
+          const index = uniqueColors.indexOf( variant.color );
+          if ( index === -1 ) { uniqueColors.push( variant.color ); }
         }
-      })
-    })
-    return uniqueColors
+      } );
+    } );
+    return uniqueColors;
   }
 
-  appliedFilter(event) {
-    let index = this.colors.indexOf(event.target.value);  // checked and unchecked value
-    if (event.target.checked)   
-        this.colors.push(event.target.value); // push in array cheked value
-    else 
-        this.colors.splice(index,1);  // removed in array unchecked value
-    
-    let colors = this.colors.length ? { color: this.colors.join(",") } : { color: null };    
-    this.colorsFilter.emit(colors);
+  appliedFilter( event ) {
+    const index = this.colors.indexOf( event.target.value );  // checked and unchecked value
+    if ( event.target.checked ) {
+      this.colors.push( event.target.value );
+    } // push in array cheked value
+    else {
+      this.colors.splice( index, 1 );
+    }  // removed in array unchecked value
+
+    const colors = this.colors.length ? { color: this.colors.join( "," ) } : { color: null };
+    this.colorsFilter.emit( colors );
   }
 
   // check if the item are selected
-  checked(item){
-    if(this.colors.indexOf(item) != -1){
+  checked( item ) {
+    if ( this.colors.indexOf( item ) !== -1 ) {
       return true;
     }
   }
