df7df942b13a5c34d14eb332670ad85fc094b9a5
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-10 10:06:26 -0400

    feat: listado basica de productos

--- src/app/shared/components/product/product-box-one/product-box-one.component.ts
+++ src/app/shared/components/product/product-box-one/product-box-one.component.ts
@@ -1,77 +1,77 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { QuickViewComponent } from "../../modal/quick-view/quick-view.component";
-import { CartModalComponent } from "../../modal/cart-modal/cart-modal.component";
-import { Product } from "../../../classes/product";
-import { ProductService } from "../../../services/tm.product.service";
+import { QuickViewComponent } from '../../modal/quick-view/quick-view.component';
+import { CartModalComponent } from '../../modal/cart-modal/cart-modal.component';
+import { Product } from '../../../classes/product';
+import { ProductService } from '../../../services/tm.product.service';
 
-@Component({
+@Component( {
   selector: 'app-product-box-one',
   templateUrl: './product-box-one.component.html',
-  styleUrls: ['./product-box-one.component.scss']
-})
+  styleUrls: [ './product-box-one.component.scss' ]
+} )
 export class ProductBoxOneComponent implements OnInit {
 
   @Input() product: Product;
-  @Input() currency: any = this.productService.Currency; // Default Currency 
-  @Input() thumbnail: boolean = false; // Default False 
-  @Input() onHowerChangeImage: boolean = false; // Default False
-  @Input() cartModal: boolean = false; // Default False
-  @Input() loader: boolean = false;
-  @Input() search: boolean = false;
+  // @Input() currency: any = this.productService.Currency; // Default Currency 
+  @Input() thumbnail = false; // Default False 
+  @Input() onHowerChangeImage = false; // Default False
+  @Input() cartModal = false; // Default False
+  @Input() loader = false;
+  @Input() search = false;
 
-  
-  @ViewChild("quickView") QuickView: QuickViewComponent;
-  @ViewChild("cartModal") CartModal: CartModalComponent;
 
-  public ImageSrc : string
+  @ViewChild( 'quickView' ) QuickView: QuickViewComponent;
+  @ViewChild( 'cartModal' ) CartModal: CartModalComponent;
 
-  constructor(private productService: ProductService) { }
+  ImageSrc: string;
+
+  constructor( private productService: ProductService ) { }
 
   ngOnInit(): void {
-    if(this.loader) {
-      setTimeout(() => { this.loader = false; }, 2000); // Skeleton Loader
+    if ( this.loader ) {
+      setTimeout( () => { this.loader = false; }, 2000 ); // Skeleton Loader
     }
   }
 
   // Get Product Color
-  Color(variants) {
+  Color( variants ) {
     const uniqColor = [];
-    for (let i = 0; i < Object.keys(variants).length; i++) {
-      if (uniqColor.indexOf(variants[i].color) === -1 && variants[i].color) {
-        uniqColor.push(variants[i].color)
+    for ( let i = 0; i < Object.keys( variants ).length; i++ ) {
+      if ( uniqColor.indexOf( variants[ i ].color ) === -1 && variants[ i ].color ) {
+        uniqColor.push( variants[ i ].color );
       }
     }
-    return uniqColor
+    return uniqColor;
   }
 
   // Change Variants
-  ChangeVariants(color, product) {
-    product.variants.map((item) => {
-      if (item.color === color) {
-        product.images.map((img) => {
-          if (img.image_id === item.image_id) {
+  ChangeVariants( color, product ) {
+    product.variants.map( ( item ) => {
+      if ( item.color === color ) {
+        product.images.map( ( img ) => {
+          if ( img.image_id === item.image_id ) {
             this.ImageSrc = img.src;
           }
-        })
+        } );
       }
-    })
+    } );
   }
 
   // Change Variants Image
-  ChangeVariantsImage(src) {
+  ChangeVariantsImage( src ) {
     this.ImageSrc = src;
   }
 
-  addToCart(product: any) {
-    this.productService.addToCart(product);
+  addToCart( product: any ) {
+    this.productService.addToCart( product );
   }
 
-  addToWishlist(product: any) {
-    this.productService.addToWishlist(product);
+  addToWishlist( product: any ) {
+    this.productService.addToWishlist( product );
   }
 
-  addToCompare(product: any) {
-    this.productService.addToCompare(product);
+  addToCompare( product: any ) {
+    this.productService.addToCompare( product );
   }
 
 }
