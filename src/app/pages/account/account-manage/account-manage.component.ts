48d3a8ec6ff8bee9148a8c8abccb197138bfea11
nairelyshz <njhs694@gmail.com>
2020-09-14 21:11:05 -0400

    admin de tienda en proceso con el nuevo diseño

--- src/app/pages/account/account-manage/account-manage.component.ts
+++ src/app/pages/account/account-manage/account-manage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from 'src/app/shared/classes/store';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { StorageService } from 'src/app/shared/services/storage.service';
+import { User } from 'src/app/shared/classes/user';
 
 @Component({
   selector: 'app-account-manage',
@@ -9,8 +12,14 @@ import { Store } from 'src/app/shared/classes/store';
 export class AccountManageComponent implements OnInit {
   stores : Store[] = [];
   active = 'profile';
-  
-  constructor() { 
+  public user: User = new User();
+
+  constructor(       
+    private storage: StorageService,
+
+    ) { 
+      this.user = this.storage.getItem( 'user' );
+      console.log(this.user);
     this.stores.push({
       name:'tienda 1',
       created_at: '02-02-2020'
