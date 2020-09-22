c3fbe16a4fc6b69721c05e48f7ddbf6526f42439
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-22 08:28:17 -0400

    feat: ShopConfig

--- src/app/pages/shops/shop-design/shop-design.component.ts
+++ src/app/pages/shops/shop-design/shop-design.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbSlideEvent, NgbSlideEventSource, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { ShopService } from '../../../shared/services/shop.service';
+import { Store } from '../../../shared/classes/store';
+import { StorageService } from '../../../shared/services/storage.service';
+import { User } from '../../../shared/classes/user';
+import { ToastrService } from 'ngx-toastr';
 @Component( {
   selector: 'app-shop-design',
   templateUrl: './shop-design.component.html',
@@ -7,14 +12,36 @@ import { NgbSlideEvent, NgbSlideEventSource, NgbCarousel } from '@ng-bootstrap/n
 } )
 export class ShopDesignComponent implements OnInit {
   @ViewChild( 'ngcarousel', { static: true } ) ngCarousel: NgbCarousel;
-  color: any;
+
+  color = '';
+  font = ''
   images = [
     { id: 0, url: '../../../assets/images/dog.png' },
     { id: 1, url: '../../../assets/images/lookbook.jpg' },
     { id: 2, url: '../../../assets/images/dog.png' }
   ];
-  constructor() {
+  private _shop: Store = {};
 
+  constructor(
+    private storage: StorageService,
+    private shopService: ShopService,
+    private toastrService: ToastrService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    const user: User = this.storage.getItem( 'user' );
+    this._shop = user.stores[ 0 ];
+  }
+
+  updateCongif(): void {
+    const data = { color: this.color, font: this.font };
+    this.shopService.config( this._shop._id, data ).subscribe( response => {
+      if ( response.success ) {
+        this.toastrService.info( response.message[ 0 ] );
+      }
+    } );
   }
 
   update( item ) {
@@ -23,8 +50,6 @@ export class ShopDesignComponent implements OnInit {
 
   delete( idItem ) {
     this.images.splice( idItem, 1 );
-  }
-  ngOnInit(): void {
   }
 
 }
