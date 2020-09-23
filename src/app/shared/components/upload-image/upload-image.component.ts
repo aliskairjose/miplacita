4cf3509974ebceca92a84536d3e7405789fb30a4
nairelyshz <njhs694@gmail.com>
2020-09-23 18:47:24 -0400

    revision  de slider para upload image

--- src/app/shared/components/upload-image/upload-image.component.ts
+++ src/app/shared/components/upload-image/upload-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ViewChildren, QueryList, AfterViewInit,ElementRef } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ViewChildren, QueryList, AfterViewInit,ElementRef, ViewContainerRef } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { Container } from '@angular/compiler/src/i18n/i18n_ast';
@@ -13,7 +13,7 @@ export class UploadImageComponent implements OnInit, AfterViewInit {
   fakeImage = '../../../../assets/images/marketplace/svg/plus-circle.svg';
   images: Array<string> = [];
   @ViewChild( 'ngcarousel' ) ngCarousel: ElementRef;
-
+  @ViewChild("carouselbox", {read: ViewContainerRef}) vc: ViewContainerRef;
   @Input() multiple = false;
   @Output() uploadImage: EventEmitter<Array<string>> = new EventEmitter<Array<string>>();
 
@@ -27,7 +27,6 @@ export class UploadImageComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(){
 
-    console.log("ngcarousel",this.ngCarousel);
     //this.ngCarousel.nativeElement.innerHTML = "Hello Angular 10!";
 
   }
@@ -75,6 +74,8 @@ export class UploadImageComponent implements OnInit, AfterViewInit {
 
   private imageBase( image: string, length: number ): void {
     this.images.push( image );
+    console.log("ngcarousel",this.ngCarousel, this.vc);
+
     if ( this.images.length === length ) {
       this.uploadImage.emit( this.images );
     }
