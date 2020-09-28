2247779cb89d5fb24018a2c8a649d7544d398658
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-28 18:40:10 -0400

    ajuste de multitienda en suscription

--- src/app/pages/shops/shop-suscription/shop-suscription.component.ts
+++ src/app/pages/shops/shop-suscription/shop-suscription.component.ts
@@ -37,39 +37,33 @@ export class ShopSuscriptionComponent implements OnInit, OnChanges {
     'Cupones de descuentos'
   ];
   enabled = false;
-  private _stores: Store[] = [];
-
   @Input() store: Store;
 
   constructor(
     private shopService: ShopService,
     private toastrService: ToastrService,
-    private storageService: StorageService,
-  ) { }
+  ) {
+    this.shopService.getPlans().subscribe( plans => {
+      this.plans = [ ...plans ];
+    } );
+  }
 
   ngOnChanges( changes: SimpleChanges ): void {
     this.getShopPlan();
   }
 
   ngOnInit(): void {
-    const user: User = this.storageService.getItem( 'user' );
-    this._stores = [ ...user.stores ];
 
     if ( this.store ) {
       this.enabled = true;
       this.getShopPlan();
     }
 
-    this.shopService.getPlans().subscribe( plans => {
-      this.plans = [ ...plans ];
-    } );
   }
 
   getShopPlan(): void {
     const params = `store=${this.store._id}`;
     this.shopService.storeList( 1, params ).subscribe( ( response ) => {
-      console.log(response.docs);
-      
       this.plan = response.docs[ 0 ].plan;
     } );
   }
@@ -79,7 +73,7 @@ export class ShopSuscriptionComponent implements OnInit, OnChanges {
   }
 
   changePlan( planId: string ): void {
-    this.shopService.updateStorePlan( this._stores[ 0 ]._id, { plan: planId } ).subscribe( response => {
+    this.shopService.updateStorePlan( this.store._id, { plan: planId } ).subscribe( response => {
       const plan = response.result.plan;
       if ( response.success ) {
         this.toastrService.info( response.message[ 0 ] );
