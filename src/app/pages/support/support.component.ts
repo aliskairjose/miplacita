1adc62aae02026df5e399bcd043729d8b48cc9ba
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-29 10:19:35 -0400

    Input en cupones

--- src/app/pages/support/support.component.ts
+++ src/app/pages/support/support.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Store } from '../../shared/classes/store';
 
-@Component({
+@Component( {
   selector: 'app-support',
   templateUrl: './support.component.html',
-  styleUrls: ['./support.component.scss']
-})
-export class SupportComponent implements OnInit {
+  styleUrls: [ './support.component.scss' ]
+} )
+export class SupportComponent implements OnInit, OnChanges {
+
+  @Input() store: Store;
 
   constructor() { }
+  ngOnChanges( changes: SimpleChanges ): void {
+    this.store = JSON.parse( sessionStorage.getItem( 'store' ) );
+    this.init();
+  }
 
   ngOnInit(): void {
+  }
+
+  private init(): void {
+
   }
 
 }
