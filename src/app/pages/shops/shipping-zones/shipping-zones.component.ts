f78129aa3ec73832fbb1b3bf28a673851813e44c
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-09-29 08:36:59 -0400

    feat: mostrando la data de zonas de envios

--- src/app/pages/shops/shipping-zones/shipping-zones.component.ts
+++ src/app/pages/shops/shipping-zones/shipping-zones.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ShopService } from '../../../shared/services/shop.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { User } from '../../../shared/classes/user';
 import { Paginate } from 'src/app/shared/classes/paginate';
+import { Store } from '../../../shared/classes/store';
+import { ShipmentOption } from '../../../shared/classes/shipment-option';
 
 @Component( {
   selector: 'app-shipping-zones',
   templateUrl: './shipping-zones.component.html',
   styleUrls: [ './shipping-zones.component.scss' ]
 } )
-export class ShippingZonesComponent implements OnInit {
+export class ShippingZonesComponent implements OnInit, OnChanges {
 
   submitted: boolean;
   zonesForm: FormGroup;
   zones = [];
-  allZones = [];
-  fields = ['Zona', 'Precio',''];
+  allZones: ShipmentOption[] = [];
+  fields = [ 'Zona', 'Precio', '' ];
   paginate: Paginate;
+
+  @Input() store: Store;
 
   constructor(
     private storage: StorageService,
@@ -27,11 +31,15 @@ export class ShippingZonesComponent implements OnInit {
   ) {
     this.createForm();
   }
+  ngOnChanges( changes: SimpleChanges ): void {
+    this.store = JSON.parse( sessionStorage.getItem( 'store' ) );
+  }
 
 
   ngOnInit(): void {
     const user: User = this.storage.getItem( 'user' );
-    this.zonesForm.value.store_id = user.stores[ 0 ]._id;
+    this.zonesForm.value.store_id = this.store._id;
+    this.loadShippingZones();
   }
 
   onSubmit(): void {
@@ -42,6 +50,12 @@ export class ShippingZonesComponent implements OnInit {
         // Logica despues de crear zone
       } );
     }
+  }
+
+  private loadShippingZones(): void {
+    this.shopService.findShipmentOptionByShop( this.store._id ).subscribe( shipments => {
+      this.allZones = [ ...shipments ];
+    } );
   }
 
   private createForm(): void {
