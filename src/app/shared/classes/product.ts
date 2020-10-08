a173486ffa5af1bcd9134952b36105740dd3d7d0
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-08 15:06:43 -0400

    feat: ajuste de producto

--- src/app/shared/classes/product.ts
+++ src/app/shared/classes/product.ts
@@ -2,13 +2,17 @@ export interface Product {
   _id?: string;
   uuid?: string;
   name?: string;
+  type?: string; // variable si esta vacio es producto base,
+  parent?: string; // Id del producto base,
+  color?: string; // Id del color
+  size?: string; // Id del tamaño,
   description?: string;
   price?: string;
   colors?: string[];
   store_id?: string;
   status?: string;
   category?: string;
-  image?: [string];
+  image?: [ string ];
   images?: Images[];
   stock?: number;
   store?: any;
