620a02b2117061b75d1975b59e36351da33645c6
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-11 13:17:13 -0400

    Mejora en emit de upload image

--- src/app/shared/components/upload-image/upload-image.component.ts
+++ src/app/shared/components/upload-image/upload-image.component.ts
@@ -75,10 +75,14 @@ export class UploadImageComponent implements OnInit, AfterViewInit {
   }
 
   private imageBase( image: string, length: number ): void {
-
+    console.log( image, length );
+    let images = [];
     this.images.push( image );
-    if ( this.images.length === length ) {
-      this.uploadImage.emit( this.images );
+    images.push( image );
+    if ( images.length === length ) {
+      console.log( 'emit' );
+      this.uploadImage.emit( images );
+      images = [];
     }
   }
 
