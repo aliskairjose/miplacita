c10fc315214387fe9da28ed4d92b79a669337fd4
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-13 10:14:16 -0400

    eliminando import sin uso

--- src/app/pages/shops/best-sellers/best-sellers.component.ts
+++ src/app/pages/shops/best-sellers/best-sellers.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit, Input, SimpleChanges, ViewChild, ElementRef, OnChanges } from '@angular/core';
 import { Paginate } from 'src/app/shared/classes/paginate';
-import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { Store } from 'src/app/shared/classes/store';
 import { Product } from 'src/app/shared/classes/product';
 import { ExportService } from 'src/app/shared/services/export.service';
 import { ProductService } from 'src/app/shared/services/product.service';
-import { CustomDateParserFormatterService } from '../../../shared/adapter/custom-date-parser-formatter.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component( {
   selector: 'app-best-sellers',
