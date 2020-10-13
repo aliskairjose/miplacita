8667664183c74269cff1ac80dfa1639cb1ef7efd
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-13 09:09:14 -0400

    style: ajuste de estilo y tslint

--- src/app/pages/shops/best-sellers/best-sellers.component.ts
+++ src/app/pages/shops/best-sellers/best-sellers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, ViewChild, ElementRef, OnChanges } from '@angular/core';
 import { Paginate } from 'src/app/shared/classes/paginate';
 import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -7,17 +7,17 @@ import { Product } from 'src/app/shared/classes/product';
 import { ExportService } from 'src/app/shared/services/export.service';
 import { ProductService } from 'src/app/shared/services/product.service';
 
-@Component({
+@Component( {
   selector: 'app-best-sellers',
   templateUrl: './best-sellers.component.html',
-  styleUrls: ['./best-sellers.component.scss']
-})
-export class BestSellersComponent implements OnInit {
+  styleUrls: [ './best-sellers.component.scss' ]
+} )
+export class BestSellersComponent implements OnInit, OnChanges {
   @ViewChild( 'TABLE', { read: ElementRef } ) table: ElementRef;
 
 
-  fields = ['ID del Producto', 'Nombre del Producto','Cantidad Vendida'];
-  bestSellers: Product[]= []
+  fields = [ 'ID del Producto', 'Nombre del Producto', 'Cantidad Vendida' ];
+  bestSellers: Product[] = [];
   paginate: Paginate;
   role: string;
   fechaIni = '';
@@ -28,14 +28,16 @@ export class BestSellersComponent implements OnInit {
   modelTo: NgbDateStruct;
   modelFrom: NgbDateStruct;
   @Input() store: Store;
+
   constructor(
     private auth: AuthService,
     private ngbCalendar: NgbCalendar,
     private productService: ProductService,
-    private exportDoc: ExportService) { }
+    private exportDoc: ExportService ) { }
 
   ngOnInit(): void {
   }
+
   ngOnChanges( changes: SimpleChanges ): void {
     this.store = JSON.parse( sessionStorage.getItem( 'store' ) );
     this.init();
@@ -47,13 +49,13 @@ export class BestSellersComponent implements OnInit {
 
     this.loadData();
   }
-  
+
   private loadData( page = 1 ): void {
     const params = `store=${this.store._id}&name=${this.name}&status=${this.status}&best=${this.order}`;
-
-
     this.productService.productList( page, params ).subscribe( result => {
       this.bestSellers = [ ...result.docs ];
+      console.log( this.bestSellers );
+
       this.paginate = { ...result };
       this.paginate.pages = [];
       for ( let i = 1; i <= this.paginate.totalPages; i++ ) {
@@ -63,17 +65,16 @@ export class BestSellersComponent implements OnInit {
     } );
   }
 
-
   setPage( page: number ) {
     this.loadData( page );
   }
 
-  ExportTOExcel( ) {
-    this.exportDoc.ExportTOExcel(this.table.nativeElement,"best-sellers-report");
+  ExportTOExcel() {
+    this.exportDoc.ExportTOExcel( this.table.nativeElement, 'best-sellers-report' );
   }
 
-  ExportTOPDF( ) {
-    this.exportDoc.ExportTOPDF('#mp-table','Productos más vendidos','best-sellers-report');
+  ExportTOPDF() {
+    this.exportDoc.ExportTOPDF( '#mp-table', 'Productos más vendidos', 'best-sellers-report' );
   }
 
 }
