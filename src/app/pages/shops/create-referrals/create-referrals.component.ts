1d44167b95951200c80aae7e4a1bf3ac06bb1cd7
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-14 21:23:18 -0400

    manejo de data de campos

--- src/app/pages/shops/create-referrals/create-referrals.component.ts
+++ src/app/pages/shops/create-referrals/create-referrals.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Store } from '../../../shared/classes/store';
 import { UserService } from '../../../shared/services/user.service';
 import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../../../environments/environment.prod';
 
 @Component( {
   selector: 'app-create-referrals',
@@ -13,8 +14,10 @@ export class CreateReferralsComponent implements OnInit, OnChanges {
 
   referralForm: FormGroup;
   submitted: boolean;
+  required = environment.errorForm.required;
 
   @Input() store: Store;
+
   constructor(
     private toastr: ToastrService,
     private userService: UserService,
@@ -33,15 +36,19 @@ export class CreateReferralsComponent implements OnInit, OnChanges {
   get f() { return this.referralForm.controls; }
 
   onSubmit(): void {
-    this.submitted = true;
-    if ( this.referralForm.valid ) {
-
-    }
+    console.log( this.referralForm.value );
+    // this.submitted = true;
+    // if ( this.referralForm.valid ) {
+    //   this.userService.updateAffiliate( this.store._id, this.referralForm.value ).subscribe( store => {
+    //     console.log( store );
+    //   } );
+    // }
   }
 
   private createForm(): void {
     this.referralForm = this.formBuilder.group( {
-
+      affiliate_program_amount: [ '', [ Validators.required ] ],
+      affiliate_program: [ '' ]
     } );
   }
 
