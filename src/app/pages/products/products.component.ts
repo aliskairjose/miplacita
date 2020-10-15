d43f6c34f4add5e78ce94c7f9656a79f3ed8260f
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-15 08:57:46 -0400

    fix: eliminando recursividad de componente

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -77,6 +77,10 @@ export class ProductsComponent implements OnChanges, AfterViewInit {
     }
   }
 
+  reload( event: boolean ): void {
+    this.reloadData();
+  }
+
   reloadData(): void {
     this.init();
   }
