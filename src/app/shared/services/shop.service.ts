3e15ef4195f0e5fa90f4ffa9cd72c95e130a8512
nairelyshz <njhs694@gmail.com>
2020-10-15 12:48:08 -0400

    fix service

--- src/app/shared/services/shop.service.ts
+++ src/app/shared/services/shop.service.ts
@@ -246,8 +246,14 @@ export class ShopService {
   getDebts( id: string, params = '' ): Observable<number> {
     return this.http.get( `debts/${id}?${params}` ).pipe(
       map( result => {
-        if ( result.success ) { return result.totalAmount[ 0 ].amount; }
-      } )
+        console.log(result);
+        if ( result.success ) { 
+          if(result.totalAmount.length){
+            return result.totalAmount[ 0 ].amount; 
+          }            
+          return 0; 
+        }
+      })
     );
   }
 
