7f437767cce71c2c7da5901e2a9caeef9c616a67
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-10-26 11:02:09 -0400

    carga de tienda por nombre

--- src/app/pages/shops/store-page/store-page.component.ts
+++ src/app/pages/shops/store-page/store-page.component.ts
@@ -6,15 +6,14 @@ import { ShopService } from 'src/app/shared/services/shop.service';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { Result } from 'src/app/shared/classes/response';
 
-@Component({
+@Component( {
   selector: 'app-store-page',
   templateUrl: './store-page.component.html',
-  styleUrls: ['./store-page.component.scss']
-})
+  styleUrls: [ './store-page.component.scss' ]
+} )
 export class StorePageComponent implements OnInit {
   products: Product[] = [];
   store: Store;
-  idStore: string;
   sliders = [];
   verticalBanners = [
     '../../../../assets/images/banner/1.jpg',
@@ -24,24 +23,24 @@ export class StorePageComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private storeService: ShopService,
-    private productService: ProductService) { }
+    private productService: ProductService ) { }
 
   ngOnInit(): void {
-    this.route.url.subscribe((url)=>{
-      this.idStore = url[ 1 ].path;
-    })
-    this.storeService.getStore(this.idStore).subscribe((response)=>{
-      console.log("store", response);
-      if(response.success){
-        this.store = response.result;
+    this.route.url.subscribe( ( url ) => {
+      const name = url[ 0 ].path.replace( '-', ' ' );
+      console.log( name )
+      this.storeService.getStoreByName( name ).subscribe( store => {
+        this.store = { ...store };
         this.sliders = this.store.config.images;
-      }
-    })
+      } );
+    } );
+
+
     this.getCollectionProducts();
   }
   private getCollectionProducts(): void {
     const params = `feature=true`;
-    this.productService.productList(1, params ).subscribe( ( result: Result<Product> ) => {
+    this.productService.productList( 1, params ).subscribe( ( result: Result<Product> ) => {
       this.products = [ ...result.docs ];
     } );
 
