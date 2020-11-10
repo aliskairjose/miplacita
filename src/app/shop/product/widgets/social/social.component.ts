5b5875fa43c09db19380d94d536a7373014e9741
nairelyshz <njhs694@gmail.com>
2020-11-10 13:27:19 -0400

    share para Whatsapp, twitter y facebook

--- src/app/shop/product/widgets/social/social.component.ts
+++ src/app/shop/product/widgets/social/social.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social.component.scss']
 })
 export class SocialComponent implements OnInit {
-
+  apiFB = 'https://www.facebook.com/sharer/sharer.php?u=' +  window.location.href;
+  apiWA = 'https://api.whatsapp.com/send?text=' +  window.location.href;
+  apiTW = 'https://twitter.com/intent/tweet?text=' + 'Mira esto!' + '&url=' + window.location.href + '&hashtags=#marketplace';
+  base = 'https://marketplace.dev.cronapis.com/';
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
