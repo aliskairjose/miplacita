41caaff0a61f631ca035205cdad30badf9bcd3d2
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-11-16 10:38:47 -0400

    Mejoras de codigo

--- src/app/pages/payment-manager/payment-manager.component.ts
+++ src/app/pages/payment-manager/payment-manager.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CreditCard } from '../../shared/classes/credit-card';
 
-@Component({
+@Component( {
   selector: 'app-payment-manager',
   templateUrl: './payment-manager.component.html',
-  styleUrls: ['./payment-manager.component.scss']
-})
+  styleUrls: [ './payment-manager.component.scss' ]
+} )
 export class PaymentManagerComponent implements OnInit {
-  card: any;
+  card: CreditCard = {};
+
   constructor() { }
 
   ngOnInit(): void {
