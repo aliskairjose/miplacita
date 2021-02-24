a32c081e3931bab9b95c7765cbee523aafbb2001
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-02-24 14:21:16 -0400

    fix: eliminando session storage

--- src/app/shop/checkout/success/success.component.ts
+++ src/app/shop/checkout/success/success.component.ts
@@ -28,18 +28,20 @@ export class SuccessComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
-    this.orderService.checkoutItems.subscribe( response => {
-      this.orderDetails = response;
-    } );
+    // tslint:disable-next-line: deprecation
+    this.orderService.checkoutItems.subscribe( response => this.orderDetails = response );
   }
 
   goTo(): void {
-    console.log( this.url );
     if ( this.url === '/shop/register/success' ) {
       const login = this.storage.getItem( 'prelogin' );
+      // tslint:disable-next-line: deprecation
       this.auth.login( login ).subscribe( data => {
         this.storage.setLoginData( 'data', data );
         this.auth.authSubject( data.success );
+        this.storage.removeItem( 'prelogin' );
+        this.storage.removeItem( 'userForm' );
+        sessionStorage.clear();
         this.router.navigate( [ '/pages/account/user/profile' ] );
       } );
     }
