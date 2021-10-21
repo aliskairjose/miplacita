2d367c1c675d18c3ee95380ebc612136a10c77ba
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-10-21 09:26:47 -0400

    questions

--- src/app/home/marketplace/marketplace.component.ts
+++ src/app/home/marketplace/marketplace.component.ts
@@ -115,59 +115,131 @@ export class MarketplaceComponent implements OnInit, OnDestroy {
     {
       id: 1,
       isCollapsed: true,
-      question: '¿Cómo Empiezo?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Qué es MiPlacita.com?',
+      answer: `Miplacita.shop es una tienda virtual que además de darte la posibilidad de vender tus productos  y/o servicios de forma online en dicha plataforma, te da las herramientas para personalizar tu propia tienda online.
+      2 posibilidades de venta desde una misma plataforma virtual, logrando llegar a más clientes.`
     },
     {
       id: 2,
       isCollapsed: true,
-      question: '¿Cómo puedo cobrar a mi cliente?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Qué podré lograr si abro una tienda virtual dentro de miplacita.shop',
+      answer: `Desde tu nueva tienda online podrás vender tus productos o servicios, promocionarlos en redes sociales, cobrarles a tus clientes, manejar tus entregas, manejar descuentos para clientes frecuentes, manejar tus informes de venta, tu inventario y mucho más.`
     },
     {
       id: 3,
       isCollapsed: true,
-      question: '¿Cómo cuesta la aplicación?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Cómo creo mi tienda virtual?',
+      answer: `Es muy fácil y divertido crear tu tienda virtual dentro de miplacita.shop.  Solo escoge tu plan, crea tu perfil, diseña tu tienda y arranca a vender. Adjunto video. `
     },
     {
       id: 4,
       isCollapsed: true,
-      question: '¿Hay alguna comisión adicional?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Cuánto cuestan los planes y que incluyen?',
+      answer: `Hay Dos planes a escoger.
+                PRIMERO- ES GRATIS
+                SEGUNDO -  $8.88 al mes (Paga anual y ahorra)
+                `
     },
     {
       id: 5,
       isCollapsed: true,
-      question: '¿Cómo puedo crear y compartir mi catálogo?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Cómo manejo mi tienda?',
+      answer: `Podrás manejar tu tienda, tus inventarios, productos y ventas desde tu computadora, tablet y/o teléfonos inteligentes. Solo debes conectarte a la web y listo.`
     },
     {
       id: 6,
       isCollapsed: true,
-      question: '¿Puedo tener tenderos propios?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Cuánto me cuesta vender en mi tienda virtual?',
+      answer: `Como sabes, las tarjetas de crédito cobran a las tiendas para poder tener un punto de venta.
+              3.4% de la venta + $0.35 por transacción.
+              De los mejores precios del mercado local e internacional.`
     },
     {
       id: 7,
       isCollapsed: true,
-      question: '¿Cómo puedo generar más ingresos?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
+      question: '¿Puedo promocionar mi tienda o productos específicos en mis redes sociales o WhatsApp?',
+      answer: ``
     },
     {
       id: 8,
       isCollapsed: true,
-      question: '¿Tengo algún soporte, si necesito ayuda?',
-      answer: 'Muy fácil! Descarga el app, Crea tu Tienda Online en solo 3 pasos y empieza a vender.' +
-        'Sólo te toma 3 minutos!' + 'Agrega tus primeros productos, empieza a compartir tu catálogo y a Vender.'
-    }
+      question: '¿Cómo vendo los productos de mi tienda virtual en la tienda principal de mi miplacita.shop ?',
+      answer: `Al crear tu tienda virtual dentro de miplacita.shop, se te preguntará si quieres promocionar y vender los productos de tu tienda en la tienda principal.
+              Al optar por la opción SI, podrás vender tus productos en nuestra tienda.  
+              Al optar por la opción NO, tus productos no se promocionarán en la tienda principal de miplacita.shop.`
+    },
+    {
+      id: 9,
+      isCollapsed: true,
+      question: '¿Qué gana la página principal al vender mis productos?',
+      answer: `Tú. Al realizarse una venta, te llegará una notificación de lo que se vendió y de los detalles de la entrega que tienes que realizar. La página será únicamente un intermediario y no se hace responsable de las entregas o de los productos.`
+    },
+    {
+      id: 10,
+      isCollapsed: true,
+      question: '¿Qué porcentaje se queda miplacita.shop si vendo en su perfil?',
+      answer: `15% de la venta.`
+    },
+    {
+      id: 11,
+      isCollapsed: true,
+      question: '¿Cómo y cuándo me pagan mis ventas?',
+      answer: `Los pagos de las ventas de tus productos y servicios, tanto en tu tienda virtual como en la página principal, se harán semanales. Cierre los domingos media noche. Pago los lunes. El dinero será transferido a la cuenta bancaria registrada al momento de la apertura de la tienda. Dicha cuenta bancaria deberá tener el nombre del dueño de la tienda de ser una empresa natural, o el nombre de la empresa de ser una empresa jurídica. No se aceptará triangulación de dinero de ningún tipo.`
+    },
+    {
+      id: 12,
+      isCollapsed: true,
+      question: 'Puedo actualizar mi cuenta de banco?',
+      answer: `Claro, solo ve a tu perfil y listo. Deberás seguir las reglas mencionadas en el punto anterior.`
+    },
+    {
+      id: 13,
+      isCollapsed: true,
+      question: '¿Cuentan con APIs para integrar con otras plataformas como Wix, Uber, etc?',
+      answer: `No, por ahora no se pueden realizar integraciones ni tenemos un API, pero está en los planes a futuro.`
+    },
+    {
+      id: 14,
+      isCollapsed: true,
+      question: '¿Puedo generar cobros recurrentes (mensualidad)?',
+      answer: ``
+    },
+    {
+      id: 15,
+      isCollapsed: true,
+      question: '¿Puedo hacer cobros por redes sociales o WhatsApp?',
+      answer: ``
+    },
+    {
+      id: 16,
+      isCollapsed: true,
+      question: '¿Puedo cobrarle a clientes en el extranjero?',
+      answer: `¡Claro Por medio de nuestra plataforma de cobro podrá cobrar a todas partes del mundo. Solo asegúrate de estipular bien tu método de entregas.`
+    },
+    {
+      id: 17,
+      isCollapsed: true,
+      question: '¿Cómo cobro las entregas?',
+      answer: `Podrás ponerles costo a las distintas zonas del país según tu criterio. Mira este video.`
+    },
+    {
+      id: 18,
+      isCollapsed: true,
+      question: '¿Cómo hago para saber si una persona ya me pagó?',
+      answer: `La plataforma emitirá una notificación cada vez que se lleve acabo con éxito una venta.`
+    },
+    {
+      id: 19,
+      isCollapsed: true,
+      question: '¿Qué método de pago se puede utilizar?',
+      answer: `Podrás pagar con tarjeta de crédito mastercard o visa`
+    },
+    {
+      id: 20,
+      isCollapsed: true,
+      question: '¿Me podrán pagar con tarjeta clave?',
+      answer: `Pronto`
+    },
   ];
 
   // beneficios
